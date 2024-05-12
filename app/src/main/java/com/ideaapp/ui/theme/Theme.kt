0e4c1e9185fa6cb8d9778836d6ff159d01a713de
RuslanGadzhiev <ghg00rus300@gmail.com>
2024-05-12 15:07:33 +0700

    fix edge to edge

--- app/src/main/java/com/ideaapp/ui/theme/Theme.kt
+++ app/src/main/java/com/ideaapp/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.ideaapp.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 
 private val DarkColorScheme = darkColorScheme(
@@ -88,6 +92,19 @@ fun IdeasAppTheme(
         }
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+
+            WindowCompat.getInsetsController(window, view).apply {
+                isAppearanceLightNavigationBars = !darkTheme
+                isAppearanceLightStatusBars = !darkTheme
+            }
+        }
     }
 
     MaterialTheme(
